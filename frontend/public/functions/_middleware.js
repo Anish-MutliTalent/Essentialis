edefca1c4aff2d7af65cda43565e56c8d8e0b943
Anish Bhattacharya <140186055+Anish-MutliTalent@users.noreply.github.com>
2025-12-23 16:24:58 +0530

    Add Pages Functions

---
+++ frontend/public/functions/_middleware.js
@@ -0,0 +1,25 @@
+export async function onRequest({ request, next }) {
+  const url = new URL(request.url);
+  const pathname = url.pathname;
+
+  const response = await next();
+
+  if (
+    pathname.includes('/dashboard/my-docs/') ||
+    pathname.includes('/zetajs') ||
+    pathname.includes('/zetaoffice')
+  ) {
+    const headers = new Headers(response.headers);
+
+    headers.set('Cross-Origin-Embedder-Policy', 'require-corp');
+    headers.set('Cross-Origin-Opener-Policy', 'same-origin');
+
+    return new Response(response.body, {
+      status: response.status,
+      statusText: response.statusText,
+      headers,
+    });
+  }
+
+  return response;
+}
