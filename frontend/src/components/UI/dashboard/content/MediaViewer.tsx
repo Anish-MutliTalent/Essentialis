2e2147f092947d46866efae1eae6f15c4a975a0b
Anish Bhattacharya <140186055+Anish-MutliTalent@users.noreply.github.com>
2025-12-23 15:03:39 +0530

    Update iframe

--- frontend/src/components/UI/dashboard/content/MediaViewer.tsx
+++ frontend/src/components/UI/dashboard/content/MediaViewer.tsx
@@ -1362,7 +1362,7 @@ const PptViewer: React.FC<{ fileUrl: string, containerClassName?: string }> = ({
       {/* Iframe */}
       <iframe
         ref={iframeRef}
-        src="/zetajs/simple.html"
+        src="/zetajs/simple"
         style={{ 
           width: '100%', 
           height: '100%', 
